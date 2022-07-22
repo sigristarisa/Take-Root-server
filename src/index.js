397de82693b2e046fc786e064ed21872615c34db
sigristarisa <arisasigrist.ch@gmail.com>
2022-07-22 16:42:55 +0200

    set up server side

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.disable("x-powered-by");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("*", (req, res) => {
+  res.json({ ok: true });
+});
+
+// Start our API server
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`\n Server is running on http://localhost:${port}\n`);
+});
